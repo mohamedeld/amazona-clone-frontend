29952179a2f9ba62c253c7c68a02efe7aa698d38
mohamedeld <mohamed.azoz20010@gmail.com>
2023-08-25 20:51:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <a>Amazona</a>
+      </header>
+      <main>list of products</main>
+    </>
+  )
+}
+
+export default App
